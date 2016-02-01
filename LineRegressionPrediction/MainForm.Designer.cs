5f7424796f4e2f1d9924b0adfe2b96e4469cc74e
Elimas <Elimas@users.noreply.github.com>
2016-02-01 19:48:44 +0100

    wczytywanie danych CSV, wyświetlanie danych jako punkty

--- LineRegressionPrediction/MainForm.Designer.cs
+++ LineRegressionPrediction/MainForm.Designer.cs
@@ -35,19 +35,30 @@
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.mainChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.drawButton = new System.Windows.Forms.Button();
+            this.groupBoxData = new System.Windows.Forms.GroupBox();
+            this.numericLearningCount = new System.Windows.Forms.NumericUpDown();
+            this.labelTestCount = new System.Windows.Forms.Label();
+            this.labelDataCount = new System.Windows.Forms.Label();
+            this.labelLearningCount = new System.Windows.Forms.Label();
+            this.labelData = new System.Windows.Forms.Label();
+            this.buttonLoadData = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.beta0Field = new System.Windows.Forms.NumericUpDown();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.beta1Field = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.beta0Field = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.drawButton = new System.Windows.Forms.Button();
+            this.openFileData = new System.Windows.Forms.OpenFileDialog();
+            this.label4 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.mainChart)).BeginInit();
             this.panel1.SuspendLayout();
+            this.groupBoxData.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericLearningCount)).BeginInit();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.beta0Field)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.beta1Field)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.beta0Field)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -87,6 +98,7 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.groupBoxData);
             this.panel1.Controls.Add(this.groupBox1);
             this.panel1.Controls.Add(this.drawButton);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -95,15 +107,75 @@
             this.panel1.Size = new System.Drawing.Size(240, 320);
             this.panel1.TabIndex = 0;
             // 
-            // drawButton
+            // groupBoxData
             // 
-            this.drawButton.Location = new System.Drawing.Point(9, 281);
-            this.drawButton.Name = "drawButton";
-            this.drawButton.Size = new System.Drawing.Size(75, 23);
-            this.drawButton.TabIndex = 4;
-            this.drawButton.Text = "Rysuj";
-            this.drawButton.UseVisualStyleBackColor = true;
-            this.drawButton.Click += new System.EventHandler(this.drawButton_Click);
+            this.groupBoxData.Controls.Add(this.label4);
+            this.groupBoxData.Controls.Add(this.numericLearningCount);
+            this.groupBoxData.Controls.Add(this.labelTestCount);
+            this.groupBoxData.Controls.Add(this.labelDataCount);
+            this.groupBoxData.Controls.Add(this.labelLearningCount);
+            this.groupBoxData.Controls.Add(this.labelData);
+            this.groupBoxData.Controls.Add(this.buttonLoadData);
+            this.groupBoxData.Location = new System.Drawing.Point(3, 3);
+            this.groupBoxData.Name = "groupBoxData";
+            this.groupBoxData.Size = new System.Drawing.Size(237, 156);
+            this.groupBoxData.TabIndex = 8;
+            this.groupBoxData.TabStop = false;
+            this.groupBoxData.Text = "Dane";
+            // 
+            // numericLearningCount
+            // 
+            this.numericLearningCount.Location = new System.Drawing.Point(143, 108);
+            this.numericLearningCount.Name = "numericLearningCount";
+            this.numericLearningCount.Size = new System.Drawing.Size(65, 20);
+            this.numericLearningCount.TabIndex = 11;
+            this.numericLearningCount.ValueChanged += new System.EventHandler(this.numericLearningCount_ValueChanged);
+            // 
+            // labelTestCount
+            // 
+            this.labelTestCount.AutoSize = true;
+            this.labelTestCount.Location = new System.Drawing.Point(6, 133);
+            this.labelTestCount.Name = "labelTestCount";
+            this.labelTestCount.Size = new System.Drawing.Size(144, 13);
+            this.labelTestCount.TabIndex = 10;
+            this.labelTestCount.Text = "Liczba w zbiorze testowym: 0";
+            // 
+            // labelDataCount
+            // 
+            this.labelDataCount.AutoSize = true;
+            this.labelDataCount.Location = new System.Drawing.Point(6, 88);
+            this.labelDataCount.Name = "labelDataCount";
+            this.labelDataCount.Size = new System.Drawing.Size(88, 13);
+            this.labelDataCount.TabIndex = 9;
+            this.labelDataCount.Text = "Liczba danych: 0";
+            // 
+            // labelLearningCount
+            // 
+            this.labelLearningCount.AutoSize = true;
+            this.labelLearningCount.Location = new System.Drawing.Point(6, 110);
+            this.labelLearningCount.Name = "labelLearningCount";
+            this.labelLearningCount.Size = new System.Drawing.Size(130, 13);
+            this.labelLearningCount.TabIndex = 8;
+            this.labelLearningCount.Text = "Liczba w zbiorze uczącym";
+            // 
+            // labelData
+            // 
+            this.labelData.AutoSize = true;
+            this.labelData.Location = new System.Drawing.Point(6, 67);
+            this.labelData.Name = "labelData";
+            this.labelData.Size = new System.Drawing.Size(72, 13);
+            this.labelData.TabIndex = 6;
+            this.labelData.Text = "Wczytaj dane";
+            // 
+            // buttonLoadData
+            // 
+            this.buttonLoadData.Location = new System.Drawing.Point(84, 62);
+            this.buttonLoadData.Name = "buttonLoadData";
+            this.buttonLoadData.Size = new System.Drawing.Size(65, 23);
+            this.buttonLoadData.TabIndex = 7;
+            this.buttonLoadData.Text = "Przeglądaj";
+            this.buttonLoadData.UseVisualStyleBackColor = true;
+            this.buttonLoadData.Click += new System.EventHandler(this.buttonLoadData_Click);
             // 
             // groupBox1
             // 
@@ -112,21 +184,43 @@
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.beta0Field);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Location = new System.Drawing.Point(3, 165);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(237, 100);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Równanie regresji";
             // 
-            // label1
+            // beta1Field
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 26);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(69, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Y = β0 + β1X";
+            this.beta1Field.DecimalPlaces = 3;
+            this.beta1Field.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.beta1Field.Location = new System.Drawing.Point(59, 72);
+            this.beta1Field.Name = "beta1Field";
+            this.beta1Field.Size = new System.Drawing.Size(65, 20);
+            this.beta1Field.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "β1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "β0";
             // 
             // beta0Field
             // 
@@ -146,36 +240,38 @@
             0,
             65536});
             // 
-            // label2
+            // label1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 48);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(19, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "β0";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Y = β0 + β1X";
             // 
-            // label3
+            // drawButton
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(7, 74);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(19, 13);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "β1";
+            this.drawButton.Location = new System.Drawing.Point(9, 281);
+            this.drawButton.Name = "drawButton";
+            this.drawButton.Size = new System.Drawing.Size(75, 23);
+            this.drawButton.TabIndex = 4;
+            this.drawButton.Text = "Rysuj";
+            this.drawButton.UseVisualStyleBackColor = true;
+            this.drawButton.Click += new System.EventHandler(this.drawButton_Click);
             // 
-            // beta1Field
+            // openFileData
             // 
-            this.beta1Field.DecimalPlaces = 3;
-            this.beta1Field.Increment = new decimal(new int[] {
-            1,
-            0,
-            0,
-            65536});
-            this.beta1Field.Location = new System.Drawing.Point(59, 72);
-            this.beta1Field.Name = "beta1Field";
-            this.beta1Field.Size = new System.Drawing.Size(65, 20);
-            this.beta1Field.TabIndex = 5;
+            this.openFileData.FileName = "openFileDialog1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(157, 39);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Format pliku: \r\nPlik CSV, kolumny oddzielone \';\'\r\nczęść dziesiętna oddzielona \',\'" +
+    "";
             // 
             // MainForm
             // 
@@ -188,10 +284,13 @@
             this.tableLayoutPanel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.mainChart)).EndInit();
             this.panel1.ResumeLayout(false);
+            this.groupBoxData.ResumeLayout(false);
+            this.groupBoxData.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericLearningCount)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.beta0Field)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.beta1Field)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.beta0Field)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -208,6 +307,15 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.NumericUpDown beta0Field;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonLoadData;
+        private System.Windows.Forms.Label labelData;
+        private System.Windows.Forms.OpenFileDialog openFileData;
+        private System.Windows.Forms.GroupBox groupBoxData;
+        private System.Windows.Forms.NumericUpDown numericLearningCount;
+        private System.Windows.Forms.Label labelTestCount;
+        private System.Windows.Forms.Label labelDataCount;
+        private System.Windows.Forms.Label labelLearningCount;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- LineRegressionPrediction/MainForm.cs
+++ LineRegressionPrediction/MainForm.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
+using System.IO;
+using System.Collections.Generic;
 
 namespace LineRegressionPrediction
 {
     public partial class MainForm : Form
     {
+        List<Point> dataSet = new List<Point>();
+        List<Point> learningSet = new List<Point>();
+        List<Point> testingSet = new List<Point>();
+
         public MainForm()
         {
             InitializeComponent();
@@ -25,6 +31,58 @@ namespace LineRegressionPrediction
             lineSeries.Points.AddXY(0, y0);
             lineSeries.Points.AddXY(1, y1);
             mainChart.Series.Add(lineSeries);
+        }
+
+        private void buttonLoadData_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = openFileData.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                dataSet.Clear();
+                var lines = File.ReadLines(openFileData.FileName);
+                foreach (var line in lines)
+                {
+                    var values = line.Split(';');
+                    dataSet.Add(new Point(double.Parse(values[0]), double.Parse(values[1])));
+                }
+            }
+            labelDataCount.Text = "Liczba danych: " + dataSet.Count;
+            numericLearningCount.Minimum = 1;
+            numericLearningCount.Maximum = dataSet.Count - 1;
+            numericLearningCount.Value = (int)(0.6f * dataSet.Count);
+            drawPoints();
+        }
+
+        private void drawPoints()
+        {
+            Series pointSeries = new Series
+            {
+                ChartType = SeriesChartType.Point,
+                Color = System.Drawing.Color.SeaGreen,
+                Name = "Punkty"
+            };
+            foreach (Point point in dataSet)
+            {
+                pointSeries.Points.AddXY(point.X, point.Y);
+            }
+            mainChart.Series.Add(pointSeries);
+        }
+
+        private void numericLearningCount_ValueChanged(object sender, EventArgs e)
+        {
+            labelTestCount.Text = "Liczba w zbiorze testowym: " + (dataSet.Count - numericLearningCount.Value);
+        }
+    }
+
+    struct Point
+    {
+        public double X;
+        public double Y;
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
         }
     }
 }
